c7408e4391b30efa502f9ce54e37f6faaced6928
Dani-Hrdz <105619891+Dani-Hrdz@users.noreply.github.com>
2022-06-09 11:39:46 -0700

    primera

---
+++ SISTEMA DE INVENTARIO/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SISTEMA_DE_INVENTARIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaracion de variables auxiliares
+            string Arch = null;
+            int option;
+            
+
+            //creacion del objeto
+            SistemaInventario Si = new SistemaInventario();
+
+            //menu de opciones
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\n---ARCHIVO BINARIO EMPLEADOS---");
+                Console.WriteLine("1.- Creacion de un Archivo");
+                Console.WriteLine("2.- Lectura de un Archivo");
+                Console.WriteLine("3.- Salida del Programa");
+                Console.Write("\nQue opcion deseas? ");
+                option = Int16.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        //bloque de escritura
+                        Si.crearArchivo();
+
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.Write("Presione <enter> para salir del Programa");
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        Console.Write("Esa Opcion no Existe!!, Presione <enter> para continuar...");
+                        Console.ReadKey();
+                        break;
+
+                }
+            } while (option != 3);
+        }
+    }
+}
---
+++ SISTEMA DE INVENTARIO/SistemaInventario.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SISTEMA_DE_INVENTARIO
+{
+    class SistemaInventario
+    {
+        string nombre, descripcion;
+        float precio;
+        int stok;
+        StreamWriter sw = null;
+        StreamReader sr = null;
+       public void crearArchivo()
+        {
+            char resp;
+            
+            try
+            {
+                sw = new StreamWriter("Productos.txt", true);
+                do
+                {
+                    Console.Clear();
+                    Console.Write("Nombre del producto: ");
+                    nombre = Console.ReadLine();
+                    Console.Write("Descripcion del producto:");
+                    descripcion = Console.ReadLine();
+                    Console.Write("Precio del producto: ");
+                    precio = Single.Parse(Console.ReadLine());
+                    Console.Write("Cantidad en stok: ");
+                    stok = int.Parse(Console.ReadLine());
+
+                    sw.Write(nombre);
+                    sw.Write(descripcion);
+                    sw.Write(precio);
+                    sw.Write(stok);
+                    Console.Write("\n\nDeseas almacenar otro registro (s/n)? ");
+
+                    resp = char.Parse(Console.ReadLine());
+
+                } while ((resp == 'S') || (resp == 's'));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nError: " + e.Message);
+                Console.WriteLine("\nRuta: " + e.StackTrace);
+            }
+            finally
+            {
+                if (sw != null) sw.Close();
+                Console.Write("\nPresione enter para terminar la Escritura de Datos y regresar al Menu.");
+                Console.ReadKey();
+            }
+        }
+       
+    }
+}
